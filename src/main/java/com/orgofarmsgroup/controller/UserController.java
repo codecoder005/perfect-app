a9cb25337ea34f22e13cec1dac106c47cb86ede5
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2022-12-07 18:48:05 +0530

    swagger removed caz unavailabity in SB 3.0.x

--- src/main/java/com/orgofarmsgroup/controller/UserController.java
+++ src/main/java/com/orgofarmsgroup/controller/UserController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/users")
 @AllArgsConstructor
 @Slf4j
