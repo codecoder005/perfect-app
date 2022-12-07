4f5b7d6aea1dd24f9313e31022ca594d0d4c521d
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2022-12-07 16:08:19 +0530

    sonarsource added (#3)
    * sonarsource added
    
    * java config added
    
    * project code changed
    
    * testcase added
    
    * triggering sonarcloud

--- src/test/java/com/orgofarmsgroup/perfectapp/PerfectAppApplicationTests.java
+++ src/test/java/com/orgofarmsgroup/perfectapp/PerfectAppApplicationTests.java
@@ -1,13 +1,18 @@
 package com.orgofarmsgroup.perfectapp;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PerfectAppApplicationTests {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(PerfectAppApplicationTests.class);
+
 	@Test
 	void contextLoads() {
+		LOGGER.info("test succeeded. triggering sonarcloud.");
 	}
 
 }
