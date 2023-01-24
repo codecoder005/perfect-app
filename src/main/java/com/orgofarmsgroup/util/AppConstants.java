5be24cb1ffb0171a202292680440c46300ac67ed
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2023-01-24 14:46:22 +0530

    code smell fixed

--- src/main/java/com/orgofarmsgroup/util/AppConstants.java
+++ src/main/java/com/orgofarmsgroup/util/AppConstants.java
@@ -5,6 +5,7 @@ public final class AppConstants {
     public static final class ApiURI {
         private ApiURI() {}
         public static final class UsersAPI {
+            private UsersAPI(){}
             public static final String USERS_ROOT = "/users";
         }
     }
