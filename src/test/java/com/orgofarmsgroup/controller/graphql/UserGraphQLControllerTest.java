e9d8ad38d29bc4e85dff31d7aad3cfbb38a430aa
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2023-01-24 12:22:39 +0530

    coverage

--- src/test/java/com/orgofarmsgroup/controller/graphql/UserGraphQLControllerTest.java
+++ src/test/java/com/orgofarmsgroup/controller/graphql/UserGraphQLControllerTest.java
@@ -1,6 +1,8 @@
 package com.orgofarmsgroup.controller.graphql;
 
 import com.orgofarmsgroup.entity.UserEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.h2.engine.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +10,11 @@ import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureG
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 
-import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @AutoConfigureGraphQlTester
+@Slf4j
 class UserGraphQLControllerTest {
     @Autowired
     private GraphQlTester graphQlTester;
@@ -30,10 +33,14 @@ class UserGraphQLControllerTest {
             }
         """;
 
-        graphQlTester.document(document)
-                .execute()
-                .path("users")
-                .entityList(UserEntity.class);
+        GraphQlTester.EntityList<UserEntity> response = graphQlTester.document(document)
+                                                                    .execute()
+                                                                    .path("users")
+                                                                    .entityList(UserEntity.class);
+        UserEntity user = response.get().get(0);
+        assertEquals(101L, user.getUid());
+        assertEquals("John", user.getName());
+        assertEquals("john@email.com", user.getEmail());
     }
 
     @Test
@@ -50,9 +57,14 @@ class UserGraphQLControllerTest {
             }
         """;
 
-        graphQlTester.document(document)
+        GraphQlTester.Entity<UserEntity, ?> responseUser = graphQlTester.document(document)
                 .execute()
                 .path("userById")
                 .entity(UserEntity.class);
+
+        UserEntity user = responseUser.get();
+        assertEquals(101L, user.getUid());
+        assertEquals("John", user.getName());
+        assertEquals("john@email.com", user.getEmail());
     }
 }
