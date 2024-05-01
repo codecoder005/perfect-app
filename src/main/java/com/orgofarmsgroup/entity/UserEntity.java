889ee5723623bb797aa4fa2f2440593a501068bc
Ramakrishna Janapureddy <85489242+codecoder005@users.noreply.github.com>
2024-05-01 20:30:56 +0530

    spring boot version upgraded to 3.x (#12)
    * spring boot version upgraded to 3.x
    
    * javax changed to jakartha

--- src/main/java/com/orgofarmsgroup/entity/UserEntity.java
+++ src/main/java/com/orgofarmsgroup/entity/UserEntity.java
@@ -1,9 +1,10 @@
 package com.orgofarmsgroup.entity;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
